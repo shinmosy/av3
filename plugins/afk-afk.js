c5340248e85cd810cc81bcb850f14892e8c1a44b
Ryumosy <114341726+shinmosy@users.noreply.github.com>
2022-10-28 03:27:18 +0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -20,7 +20,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     user.afkReason = text
     let caption = `*${conn.getName(m.sender)}* @${m.sender.split("@")[0]} *Sekarang lagi AFK*\n*Dengan Alasan*${text ? ': ' + text : ''}`
     let kataafk = ['mau turu', 'mau nyolong', 'Ke rumah ayang', 'jagain lilin', 'beli pop es', 'kawin lari', 'main kelereng', 'petak umpet', 'push renk', 'push up joni', 'olahraga', 'onani', 'beraq', 'open bo', 'di suruh emak', 'kerja']
-    conn.sendButton(m.chat, caption, wm, null, [['Ikut Afk', '.afk ' + kataafk.getRandom()]], m, { mentions: conn.parseMention(caption) })
+    conn.sendButton(m.chat, caption, wm, null, [['Ikut AFK', '.afk ' + kataafk.getRandom()]], m, { mentions: conn.parseMention(caption) })
 }
 
 handler.help = ['afk [alasan]']
