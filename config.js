32d56437a68a5365c1df8b58f70de91d45b4b561
Ryumosy <114341726+shinmosy@users.noreply.github.com>
2022-11-04 13:53:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -179,7 +179,7 @@ global.flaaa = [
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text=',
 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&fillColor1Color=%23f2aa4c&fillColor2Color=%23f2aa4c&fillColor3Color=%23f2aa4c&fillColor4Color=%23f2aa4c&fillColor5Color=%23f2aa4c&fillColor6Color=%23f2aa4c&fillColor7Color=%23f2aa4c&fillColor8Color=%23f2aa4c&fillColor9Color=%23f2aa4c&fillColor10Color=%23f2aa4c&fillOutlineColor=%23f2aa4c&fillOutline2Color=%23f2aa4c&backgroundColor=%23101820&text=']
 global.hwaifu2 = ['https://telegra.ph/file/3e60b028b1de583ec0d3d.jpg',
-'https://']//////
+'hhttps://telegra.ph/file/d50e9c8402cff236af979.jpg']//////
 /////////
 /*============== HENTAI ==============*/ 
 //OPTIONS
